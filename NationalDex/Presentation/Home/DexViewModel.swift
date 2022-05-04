c3939416438d4400d42fcde91f1f7cf01cec7b1c
mferreira-dev <martin.unix@protonmail.com>
2022-05-04 20:17:03 +0100

    Implemented MVVM and Clean Architecture

---
+++ NationalDex/Presentation/Home/DexViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModel.swift
+//  NationalDex
+//
+//  Created by Martin Ferreira on 5/4/22.
+//
+
+import Foundation
+
+final class DexViewModel {
+	
+	init() {
+		
+	}
+
+}
---
+++ NationalDex/Utils/K.swift
@@ -0,0 +1,20 @@
+//
+//  K.swift
+//  NationalDex
+//
+//  Created by Martin Ferreira on 5/4/22.
+//
+
+import Foundation
+
+struct K {
+	
+	static let greet = "Hello, world!"
+	
+	struct Cells {
+		
+		static let pokemonCell = "pokemonCell"
+		
+	}
+
+}
